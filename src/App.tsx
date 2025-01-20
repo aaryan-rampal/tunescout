3be347a9b6d9bf5ffb32c7d8d4e3dbdc4874756f
Aaryan Rampal <76913929+aaryan-rampal@users.noreply.github.com>
2025-01-20 02:29:07 -0800

    Test Hashrouter for gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
+// import {
+//   Route,
+//   RouterProvider,
+//   createBrowserRouter,
+//   createRoutesFromElements,
+// } from "react-router-dom";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// import Callback from "./pages/Callback";
+// import Dashboard from "./pages/Dashboard";
+// import Home from "./pages/Home";
+// import RootLayout from "./layouts/Layout";
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<RootLayout />}>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/callback" element={<Callback />} />
+//       <Route path="/dashboard" element={<Dashboard />} />
+//     </Route>
+//   ),
+//   {
+//     basename: "/tunescout"
+//   }
+// );
+
+// const queryClient = new QueryClient();
+
+// const App = () => {
+//   return (
+//     <QueryClientProvider client={queryClient}>
+//       <RouterProvider router={router} />
+//     </QueryClientProvider>
+//   );
+// };
+
+// export default App;
+
 import {
   Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
+  Routes,
+  HashRouter
 } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -11,25 +48,20 @@ import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import RootLayout from "./layouts/Layout";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route path="/" element={<Home />} />
-      <Route path="/callback" element={<Callback />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Route>
-  ),
-  {
-    basename: "/tunescout"
-  }
-);
-
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<RootLayout />}>
+            <Route index element={<Home />} />
+            <Route path="callback" element={<Callback />} />
+            <Route path="dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </HashRouter>
     </QueryClientProvider>
   );
 };
