df1f5d6d83e54a872a0662184571d7022a7648bb
Aaryan Rampal <76913929+aaryan-rampal@users.noreply.github.com>
2025-01-20 02:32:07 -0800

    Roll back changes
    I want to change callback to be cleaner anyways, and try to get this to
    a one page website. No need for hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,8 @@
-// import {
-//   Route,
-//   RouterProvider,
-//   createBrowserRouter,
-//   createRoutesFromElements,
-// } from "react-router-dom";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-// import Callback from "./pages/Callback";
-// import Dashboard from "./pages/Dashboard";
-// import Home from "./pages/Home";
-// import RootLayout from "./layouts/Layout";
-
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route path="/" element={<RootLayout />}>
-//       <Route path="/" element={<Home />} />
-//       <Route path="/callback" element={<Callback />} />
-//       <Route path="/dashboard" element={<Dashboard />} />
-//     </Route>
-//   ),
-//   {
-//     basename: "/tunescout"
-//   }
-// );
-
-// const queryClient = new QueryClient();
-
-// const App = () => {
-//   return (
-//     <QueryClientProvider client={queryClient}>
-//       <RouterProvider router={router} />
-//     </QueryClientProvider>
-//   );
-// };
-
-// export default App;
-
 import {
   Route,
-  Routes,
-  HashRouter
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
 } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -48,20 +11,25 @@ import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import RootLayout from "./layouts/Layout";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/callback" element={<Callback />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Route>
+  ),
+  {
+    basename: "/tunescout"
+  }
+);
+
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<RootLayout />}>
-            <Route index element={<Home />} />
-            <Route path="callback" element={<Callback />} />
-            <Route path="dashboard" element={<Dashboard />} />
-          </Route>
-        </Routes>
-      </HashRouter>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   );
 };
