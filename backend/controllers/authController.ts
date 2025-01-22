2f1bec8387db51765d6576d5c087d9b02446513b
Aaryan Rampal <76913929+aaryan-rampal@users.noreply.github.com>
2025-01-21 23:27:06 -0800

    Add auth code for backend

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -35,7 +35,7 @@ export const loginUser = async (req: Request, res: Response) => {
     // Generate JWT token (valid for 7 days)
     const token = jwt.sign({ id: user.id, email: user.email }, SECRET_KEY, { expiresIn: "7d" });
 
-    res.status(200).json({ message: "Login successful", token });
+    res.status(200).json({ success: true, message: "Login successful", token });
 
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -72,7 +72,7 @@ export const registerUser = async (req: Request, res: Response) => {
       },
     });
 
-    res.status(201).json({ message: "User registered successfully" });
+    res.status(201).json({success: true, message: "User registered successfully" });
 
   } catch (error) {
     res.status(500).json({ message: error.message });
