d414084ece23abf37c28ffeac009403ed5d26fd6
Aaryan Rampal <76913929+aaryan-rampal@users.noreply.github.com>
2025-05-23 23:44:57 -0700

    Replace TypeScript backend with FastAP

--- backend/routes/spotifyRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as spotifyController from "../controllers/spotifyController";
-
-export const spotifyRoutes = Router();
-
-spotifyRoutes.post("/get_playlists", spotifyController.getUserPlaylists);
-spotifyRoutes.post("/generate_playlist", spotifyController.generatePlaylist);
-spotifyRoutes.post("/create_playlist", spotifyController.createPlaylist);
